76009208b0c3f4dc87f6a1ff808bbb84e5f43192
Mumblecore Klotz <mklotz@pr-m5.kzn.21-school.ru>
2020-10-06 03:44:55 +0300

    Mini fix

--- srcs/managers/execute_manager.c
+++ srcs/managers/execute_manager.c
@@ -6,7 +6,7 @@
 /*   By: mklotz <mklotz@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/05 21:32:33 by mklotz            #+#    #+#             */
-/*   Updated: 2020/10/06 03:11:34 by mklotz           ###   ########.fr       */
+/*   Updated: 2020/10/06 03:42:32 by mklotz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,5 +54,8 @@ void	execute_managers(t_main *main, char *cmd)
 	main->com->args = ft_split(cmd, ' ');
 	patch = get_command_patch(main, main->com->args[0]);
 	if (hook_my_functions(main) == 0)
+	{
+		main->com->command = patch;
 		execute_another_function(main);
+	}
 }
